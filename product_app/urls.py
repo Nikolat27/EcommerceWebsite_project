9b74a12655d2cad3e1f5a55bc9757cb2c067cfac
SamAlizadeh <samalizadeh910@gmail.com>
2023-10-10 23:14:36 +0330

    I added comment and reply system

--- product_app/urls.py
+++ product_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "product_app"
 urlpatterns = [
     path("detail/<str:slug>", views.product_detail, name="detail"),
+    path("add_review/<int:pk>", views.add_comment, name="add_review"),
 ]
--- product_app/views.py
+++ product_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Color
+from .models import Product, Color, Comment
 
 
 # Create your views here.
@@ -9,8 +9,30 @@ def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     # related_products = Product.objects.filter(category__title=product.category.first().title)[:8]
     related_products = Product.objects.filter(category__in=product.category.all()).distinct()
-    return render(request, "product_app/product_detail.html", context={"product": product, "related_products": related_products})
+    return render(request, "product_app/product_detail.html",
+                  context={"product": product, "related_products": related_products})
 
 
-def add_comment(request):
-    pass
+def add_comment(request, pk):
+    if request.method == "POST":
+        name = request.POST.get("f_name")
+        body = request.POST.get("body")
+        product = Product.objects.get(id=pk)
+        pros = request.POST.get("pros")
+        cons = request.POST.get("cons")
+        author = request.user
+        rating = request.POST.get("rating")
+        parent_id = request.POST.get("parent_id")
+        related_products = Product.objects.filter(category__in=product.category.all()).distinct()
+        if parent_id:
+            comment = Comment.objects.create(name=name, body=body, product=product, pros=pros,
+                                             cons=cons, author=author, rating=rating, parent_id=parent_id)
+            return render(request, "product_app/product_detail.html", context={"product": product, "related_product":
+                                                                               related_products})
+        else:
+            parent_id = None
+            comment = Comment.objects.create(name=name, body=body, product=product, pros=pros,
+                                             cons=cons, author=author, rating=rating)
+            return render(request, "product_app/product_detail.html", context={"product": product, "related_product":
+                                                                               related_products})
+
